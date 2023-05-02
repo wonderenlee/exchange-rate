455cf73f16a5d0341d8c07f92d67647bfe993e7d
wonderenlee <wonderenlee@gmail.com>
2023-05-02 14:28:37 -0400

    clean pu

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ const swap = document.getElementById('swap');
 function calculate() {
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
-
     fetch("https://open.exchangerate-api.com/v6/latest");
         .then(res => res.json())
         .then(data => {
             // console.log(data);
             const rate = data.rates[currency_two] / data.rates[currency_one];
-
             rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-
             amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
         });
 }
